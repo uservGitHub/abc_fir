897f089bb31d545549f9761ec77444b2c5dcf4ce
uservGitHub <userv@126.com>
2018-05-09 17:45:06 +0800

    初始化

---
+++ gxd.book/app/src/main/kotlin/gxd/book/temp/TMainActivity.kt
@@ -0,0 +1,13 @@
+package gxd.book.temp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by work on 2018/5/9.
+ */
+class TMainActivity:AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ gxd.book/app/src/test/java/gxd/book/Test1.kt
@@ -0,0 +1,47 @@
+/*
+package gxd.book
+import android.content.Intent
+import android.widget.Button
+import android.widget.TextView
+import gxd.book.temp.TMainActivity
+import org.junit.Test
+
+
+
+import org.junit.Before
+
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.Shadows
+import org.robolectric.Shadows.shadowOf
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Assert
+import org.junit.Assert.*
+import org.robolectric.annotation.Config
+//import org.robolectric.shadow.api.Shadow
+import org.robolectric.shadows.ShadowActivity
+import org.robolectric.shadows.ShadowIntent
+import org.robolectric.RobolectricTestRunner
+
+*/
+/**
+ * Created by work on 2018/5/9.
+ *//*
+
+
+@RunWith(RobolectricTestRunner::class)
+@Config(constants=BuildConfig::class, sdk = intArrayOf(21))
+
+class Test1{
+    lateinit var mainActivity: TMainActivity
+    @Before
+    fun init(){
+        mainActivity = Robolectric.setupActivity(TMainActivity::class.java)
+    }
+    @Test
+    fun activityNotNull(){
+        assertNotNull(mainActivity)
+    }
+}*/
